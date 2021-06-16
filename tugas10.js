b1a90b6ab367083d93dcef0e915c02f6c93daeb4
Nur Rahmadani <nagamas.rahma@gmail.com>
2021-06-16 20:47:27 +0800

    TUGAS 10 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas10.js
@@ -0,0 +1,16 @@
+var a = 1;
+var b = a+2;
+var c = b+3;
+var d = c+4;
+var e = d+5;
+var f = e+6;
+var g = f+7;
+var h = g+8;
+var i = h+9;
+var j = i+10;
+
+var array=[a,b,c,d,e,f,g,h,i,j]
+
+for(let x of array){
+  console.log(x);
+}
